a37762768c303693dd3b2285cf97c30f16f0ed19
Vasyl Mudryk <vasyl.mudryk@omnigon.com>
2020-02-23 19:25:45 +0200

    L3_m5 Null argument possibility added into HelperBase.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase{
   app.getNavigationHelper().gotoGroupPage();
   app.getGroupHelper().selectGroup();
   app.getGroupHelper().initGroupModification();
-  app.getGroupHelper().fillGroupForm(new GroupData("testGroup", "testGroup", "testGroup2", "testGroup3"));
+  app.getGroupHelper().fillGroupForm(new GroupData("testGroup", "testGroup", "testGroup2", null));
   app.getGroupHelper().submitGroupModification();
   app.getGroupHelper().returnToGroupPage();
 }
